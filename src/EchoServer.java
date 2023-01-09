6c1652453916885d045994c430d60bc70a705c14
Lazar Raul <raullazar23@gmail.com>
2023-01-09 10:47:34 +0200

    Initial commit

---
+++ src/EchoServer.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.net.*;
+
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(4444);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 4444.");
+            System.exit(1);
+        }
+
+        Socket clientSocket = null;
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.err.println("Accept failed.");
+            System.exit(1);
+        }
+
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null) {
+            out.println(inputLine);
+        }
+
+        out.close();
+        in.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+}
